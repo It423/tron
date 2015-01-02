0514499d2bd8adfcc81a5c87298016556b34a12a
It423 <it42@hotmail.co.uk>
2015-01-02 20:54:10 +0000

    Start game as console appliation to allow users to see how to play and select features

--- Tron/Application/Application/Program.cs
+++ Tron/Application/Application/Program.cs
@@ -1,8 +1,11 @@
 // Program.cs
 // <copyright file="Program.cs"> This code is protected under the MIT License. </copyright>
+using System;
+using Tron;
+
 namespace Application
 {
-#if WINDOWS || XBOX
+#if WINDOWS
     /// <summary>
     /// The main program.
     /// </summary>
@@ -13,6 +16,81 @@ namespace Application
         /// </summary>
         /// <param name="args"> Any arguments/commands that the program is run/compiled with. </param>
         public static void Main(string[] args)
+        {
+            Help();
+            GetCommand();
+        }
+
+        /// <summary>
+        /// Prints the help message into the console.
+        /// </summary>
+        public static void Help()
+        {
+            Console.WriteLine("Welcome to tron!\n");
+            Console.WriteLine("How to play:");
+            Console.WriteLine("The aim of the game is to drive your car without crashing.");
+            Console.WriteLine("While driving you leave a train of which will smash your car if driven into.");
+            Console.WriteLine("Be the last car driving!\n");
+            Console.WriteLine("Controls:");
+            Console.WriteLine("W to go up");
+            Console.WriteLine("A to go left");
+            Console.WriteLine("S to go down");
+            Console.WriteLine("D to go right");
+            Console.WriteLine("Left shift to boost");
+            Console.WriteLine("When playing local multiplayer your opponent will use arrow keys and enter\n\n");
+            Console.WriteLine("To play local multiplayer type 'local'");
+            Console.WriteLine("To display this message again type 'help'");
+            Console.WriteLine("To quit the game type 'quit'");
+        }
+
+        /// <summary>
+        /// Gets and executes a command.
+        /// </summary>
+        public static void GetCommand()
+        {
+            while (true)
+            {
+                // Get a command
+                Console.Write("Enter a command... ");
+                string inp = Console.ReadLine();
+
+                if (inp.ToLower() == "local")
+                {
+                    StartLocalGame();
+                }
+                else if (inp.ToLower() == "help")
+                {
+                    Help();
+                }
+                else if (inp.ToLower() == "quit")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("That is not a valid command!");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Starts a game of local multiplayer tron.
+        /// </summary>
+        public static void StartLocalGame()
+        {
+            // Setup the game data
+            GameData.LocalTwoPlayer = true;
+            GameData.Tron = new TronGame(2);
+            GameData.Tron.InitializeGame();
+
+            // Start the game
+            StartGame();
+        }
+
+        /// <summary>
+        /// Starts the game.
+        /// </summary>
+        public static void StartGame()
         {
             using (Game game = new Game())
             {
