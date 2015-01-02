e5974a393e8b403152b5568784fafb7b8b4fe0cb
It423 <it42@hotmail.co.uk>
2015-01-02 08:59:32 +0000

    Fix styling rules

--- Tron/Tron/Program.cs
+++ Tron/Tron/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Program.cs
+// <copyright file="Program.cs"> This code is protected under the MIT License. </copyright>
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +8,16 @@ using System.Threading.Tasks;
 
 namespace Tron
 {
-    class Program
+    /// <summary>
+    /// The main program (for testing at the moment).
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the program.
+        /// </summary>
+        /// <param name="args"> Any arguments/commands that the program is run/compiled with. </param>
+        public static void Main(string[] args)
         {
         }
     }
