a2d01f972ffef082f0bd6032942c010b7728ce62
It423 <it42@hotmail.co.uk>
2015-01-02 11:29:15 +0000

    Add a direction enumeration

---
+++ Tron/Tron/Car/Direction.cs
@@ -0,0 +1,30 @@
+﻿// Direction.cs
+// <copyright file="Direction.cs"> This code is protected under the MIT License. </copyright>
+namespace Tron
+{
+    /// <summary>
+    /// An enumeration to represent the possible directions a car can travel.
+    /// </summary>
+    public enum Direction
+    {
+        /// <summary>
+        /// The car is moving up.
+        /// </summary>
+        Up = 0,
+
+        /// <summary>
+        /// The car is moving right.
+        /// </summary>
+        Right = 1,
+
+        /// <summary>
+        /// The car is moving down.
+        /// </summary>
+        Down = 2,
+
+        /// <summary>
+        /// The car is moving left.
+        /// </summary>
+        Left = 3
+    }
+}
