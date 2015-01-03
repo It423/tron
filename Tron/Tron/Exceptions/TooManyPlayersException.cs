137a9c43ac0c6d4824c0a5fb7dd036ca8fd8d6ed
It423 <it42@hotmail.co.uk>
2015-01-03 12:26:01 +0000

    Add too many players exception

---
+++ Tron/Tron/Exceptions/TooManyPlayersException.cs
@@ -0,0 +1,19 @@
+﻿// TooManyPlayersException.cs
+// <copyright file="TooManyPlayersException.cs"> This code is protected under the MIT License. </copyright>
+using System;
+
+namespace Tron.Exceptions
+{
+    /// <summary>
+    /// An exception thrown when too many players are in the game.
+    /// </summary>
+    public class TooManyPlayersException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TooManyPlayersException" /> class.
+        /// </summary>
+        public TooManyPlayersException()
+        { 
+        }
+    }
+}
