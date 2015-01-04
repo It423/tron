81dc2d6768c744472291778a702ad6b3986f004e
It423 <it42@hotmail.co.uk>
2015-01-04 13:19:54 +0000

    Add networking library

---
+++ Tron/Networking/Extensions/NetCarExtensions.cs
@@ -0,0 +1,54 @@
+﻿// NetCarExtensions.cs
+// <copyright file="NetCarExtensions.cs"> This code is protected under the MIT License. </copyright>
+using Tron.CarData;
+
+namespace Networking.Extensions
+{
+    /// <summary>
+    /// An extension class for the car containing useful tools for a networked game of tron.
+    /// </summary>
+    public static class NetCarExtensions
+    {
+        /// <summary>
+        /// Turns the car's important information into a byte array.
+        /// </summary>
+        /// <param name="c"> The car. </param>
+        /// <returns> The byte array of information. </returns>
+        public static byte[] ToByteArray(this Car c)
+        {
+            // There will be 4 bytes in the car's byte data
+            byte[] bArr = new byte[4];
+
+            // Add the id, x position and y position
+            bArr[0] = (byte)c.ID;
+            bArr[3] = (byte)c.Y;
+
+            // Add the x position (takes up 2 bytes)
+            int[] xVal = new int[2];
+            xVal[0] = c.X;
+            xVal[1] = 0;
+            if (xVal[0] > 255)
+            {
+                xVal[0] = 255;
+                xVal[1] = c.X - 255;
+            }
+
+            bArr[1] = (byte)xVal[0];
+            bArr[2] = (byte)xVal[1];
+
+            return bArr;
+        }
+
+        /// <summary>
+        /// Sets the car's position from a byte array.
+        /// </summary>
+        /// <param name="c"> The car. </param>
+        /// <param name="byteArray"> The byte array. </param>
+        public static void FromByteArray(this Car c, byte[] byteArray)
+        {
+            // Take the x and y position
+            c.X = byteArray[1] + byteArray[2];
+            c.Y = byteArray[3];
+        }
+    }
+}
---
+++ Tron/Networking/Extensions/NetGameExtension.cs
@@ -0,0 +1,26 @@
+﻿// NetGameExtensions.cs
+// <copyright file="NetGameExtensions.cs"> This code is protected under the MIT License. </copyright>
+using Tron;
+
+namespace Networking.Extensions
+{
+    /// <summary>
+    /// An extension class for the game containing useful tools for a networked game of tron.
+    /// </summary>
+    public static class NetGameExtensions
+    {
+        /// <summary>
+        /// Adjusts a car in the game accordingly to a byte array.
+        /// </summary>
+        /// <param name="g"> The game. </param>
+        /// <param name="byteArray"> The byte array. </param>
+        public static void SetCarFromByteArray(this TronGame g, byte[] byteArray)
+        {
+            // Get the index of the car
+            int index = byteArray[0];
+
+            // Adjust the car 
+            g.Cars[index].FromByteArray(byteArray);
+        }
+    }
+}
