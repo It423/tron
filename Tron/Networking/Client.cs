88faaa2dad635104b910d626e3a6dd1fdb6c3ec7
It423 <it42@hotmail.co.uk>
2015-01-04 13:46:48 +0000

    Add client and server class

---
+++ Tron/Networking/Client.cs
@@ -0,0 +1,16 @@
+﻿// Client.cs
+// <copyright file="Client.cs"> This code is protected under the MIT License. </copyright>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Networking
+{
+    /// <summary>
+    /// A class that acts as a client for a networked game of tron.
+    /// </summary>
+    public class Client
+    {
+    }
+}
---
+++ Tron/Networking/Server.cs
@@ -0,0 +1,16 @@
+﻿// Server.cs
+// <copyright file="Server.cs"> This code is protected under the MIT License. </copyright>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Networking
+{
+    /// <summary>
+    /// A class that acts as a server for a networked game of tron.
+    /// </summary>
+    public class Server
+    {
+    }
+}
