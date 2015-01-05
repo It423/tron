5f8e61995cde87520b04774e378b2ed797e5c363
It423 <it42@hotmail.co.uk>
2015-01-05 10:49:46 +0000

    Write properties and constructor for server

--- Tron/Networking/Server.cs
+++ Tron/Networking/Server.cs
@@ -3,6 +3,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 namespace Networking
@@ -16,5 +18,45 @@ namespace Networking
         /// The port used by the server.
         /// </summary>
         public static readonly int Port = 22528;
+
+        public Server()
+        {
+            this.Host = new UdpClient(new IPEndPoint(IPAddress.Any, Server.Port));
+            this.PlayerIPs = new IPEndPoint[12];
+        }
+
+        /// <summary>
+        /// The host socket.
+        /// </summary>
+        public UdpClient Host { get; set; }
+
+        /// <summary>
+        /// The array of player ip addresses.
+        /// </summary>
+        public IPEndPoint[] PlayerIPs { get; set; }
+
+        /// <summary>
+        /// Listens for an input.
+        /// </summary>
+        public void Listen
+        {
+
+        }
+
+        /// <summary>
+        /// Sends a message to all clients.
+        /// </summary>
+        public void SendToAll()
+        { 
+
+        }
+
+        /// <summary>
+        /// Removes a player from the game.
+        /// </summary>
+        public void RemovePlayer
+        {
+            
+        }
     }
 }
