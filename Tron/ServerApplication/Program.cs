e1255bec7b9b95bf65409d88e69c704207b37f43
It423 <it42@hotmail.co.uk>
2015-01-06 17:30:22 +0000

    Start writing server application

--- Tron/ServerApplication/Program.cs
+++ Tron/ServerApplication/Program.cs
@@ -1,15 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Program.cs
+// <copyright file="Program.cs"> This code is protected under the MIT License. </copyright>using System;
+using Networking;
+using Tron;
 
 namespace ServerApplication
-{
+{    
+    /// <summary>
+    /// The main program.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// <param name="args"> Any arguments/commands that the program is run/compiled with. </param>
         static void Main(string[] args)
         {
+            TronData.Tron = new TronGame(0, 5);
+            Server s = new Server();
+            s.Listen();
         }
     }
 }
